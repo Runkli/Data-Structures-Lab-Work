64226dbf6f1f220a36ee4587d7bae42f74ba2223
Ilknur <ilknuri607@gmail.com>
2019-11-25 09:53:51 +0200

    Update priorityqueue.c

--- queue/priorityqueue.c
+++ queue/priorityqueue.c
@@ -1,111 +1,89 @@
-#include <stdio.h>
+#include<stdio.h>
 
 struct Node{
 	int val;
-	struct Node* next;
 	int priority;
+	struct Node* next;
 };
 
 struct Queue{
 	struct Node* front;
 	struct Node* rear;
-	int size;	
+	int size;
 };
 
-
-struct Queue* createQueue() 
-{ 
-    struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue));
-    q->front = NULL;
-	q->rear = q->front; 
-    return q; 
-} 
-
-struct Node* newNode(){
-	int k, priority;
-	printf("Enter value, priority: ");
-	scanf("%d %d",&k,&priority);
+void enq(struct Queue* q){
 	
 	struct Node* temp = malloc(sizeof(struct Node));
-	temp->val = k;
+	printf("Enter value and priority: ");
+	scanf("%d %d",&temp->val,&temp->priority);
 	temp->next = NULL;
-	temp->priority = priority;
-	
-	return temp;
-}
-
-
-void enq(struct Queue* q) { 
-
-	struct Node* holder;
-	struct Node* temp = newNode();
-	struct Node* p = q->front;
-	
-	int count = 0;
-	if(q->front == NULL){
-		q->front = temp;
-		q->rear = q->front;
+	if(q->front == NULL && q->rear == NULL){
+		q->front = q->rear = temp;
 		return;
-	}else{
-		printf("Not empty\n");
 	}
-	
-    
-    while(p->next!=NULL && temp->priority < p->next->priority){
-    	p = p->next;
-	}
-	
-	holder = p->next;
-	temp->next = holder;
-	p->next = temp;
-    return;
-    
-  	p = temp;
-  	q->front = p;
-  	q->rear = q->front;
-    
-} 
-
-struct QNode* deq(struct Queue* q){
-   	
- 	printf("Dequeued: %d\n",q->front->val);  	
-    struct QNode* temp = q->front;
-    free(temp);
-  	
-    q->front = q->front->next;
-  	
-    if (q->front == NULL) 
-        q->rear = NULL; 
-    return temp;
-} 
-
-void displayQueue(struct Queue* q){
-	struct Node *p;
-
-	p = q->front;
-	printf("List content:\n");
-
-	while (p != NULL)
-	{
-		printf("--> %d\t", p->val);
-		p = p->next;
-	}
-	printf("\n");
+	q->rear->next = temp;
+	q->rear = temp;
 }
 
+void sortedenq(struct Queue* q){
+	struct Node* curr;
+	struct Node* new_node = malloc(sizeof(struct Node));
+	printf("Enter value and priority: ");
+	scanf("%d %d",&new_node->val, &new_node->priority);
+	
+	if(q->front == NULL || q->front->priority >= new_node->priority){
+		new_node->next = q->front;
+		q->front = new_node;
+		return;
+	}
+	curr = q->front;
+	while(curr->next!=NULL && curr->next->priority < new_node->priority){
+		curr = curr->next;
+	}
+	
+	new_node->next = curr->next;
+	curr->next = new_node;
+	
+	
+}
+
+void deq(struct Queue* q){
+	struct Node* temp = q->front;
+	if(q->front==NULL) return;
+	printf("Dequeued item is: %d\n",q->front->val);
+	if(q->front==q->rear){
+		q->front = q->rear = NULL;
+	}else{
+		q->front = q->front->next;
+	}
+	free(temp);
+	
+}
+
+void display(struct Queue* q){
+	struct Node* head = malloc(sizeof(struct Node));
+	head = q->front;
+	while(head!=NULL){
+		printf("val is %d\n",head->val);
+		head = head->next;
+	}
+	
+}
 
 void main(){
-	struct Queue* qHead;
-	qHead = createQueue();
+	struct Queue* q = malloc(sizeof(struct Queue));
+	q->front = malloc(sizeof(struct Node));
+	q->front = NULL;
+	q->rear = NULL;
+	q->size = 0;
 	
-	enq(qHead);
-	enq(qHead);
-	enq(qHead);
-	enq(qHead);
-	
-	displayQueue(qHead);
-	/*deq(qHead);
-	deq(qHead);
-	deq(qHead);
-	deq(qHead);*/
+	enq(q);
+	sortedenq(q);
+	sortedenq(q);
+	sortedenq(q);
+	display(q);
+	deq(q);
+	deq(q);
+	deq(q);
 }
