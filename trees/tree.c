7bea9a1983f42f020b58eeb63b8d5494a1b65392
Ilknur <ilknuri607@gmail.com>
2019-11-29 16:29:11 +0200

    Update tree.c

--- trees/tree.c
+++ trees/tree.c
@@ -7,18 +7,18 @@ struct node{
 };
 
 
-struct node *newNode(int item) 
-{ 
-    struct node *temp =  (struct node *)malloc(sizeof(struct node)); 
-    temp->val = item; 
-    temp->left = temp->right = NULL; 
-    return temp; 
-} 
+struct node *newNode(int item)
+{
+    struct node *temp =  malloc(sizeof(struct node));
+    temp->val = item;
+    temp->left = temp->right = NULL;
+    return temp;
+}
 
 void addleaf(struct node* parent){
-	
+
 	char path;
-	
+
 	printf("Value at node: %d\n",parent->val);
 	if(parent->left==NULL && parent->right==NULL){
 		printf("Add value to left and right branch: ");
@@ -28,21 +28,21 @@ void addleaf(struct node* parent){
 		parent->left->right = NULL;
 		parent->right->left = NULL;
 		parent->right->right = NULL;
-		
+
 		scanf("%d %d",&parent->left->val, &parent->right->val);
         return;
 	}
-	
+
 	fflush(stdin);
 	printf("Traverse left or right: ");
 	scanf("%c",&path);
 	if(path=='r'){
 		addleaf(parent->right);
-		
+
 	}
 	else if(path=='l'){
 		addleaf(parent->left);
-		
+
 	}
 
 }
@@ -62,65 +62,133 @@ void traverse(struct node* head){
 }
 
 
-void insertbst(struct node* head,int x){
-	
-	
-	if(x<head->val){
-		if(head->left!=NULL){
-			insertbst(head->left,x);
-		}
-		printf("smaller than curr node\n");
-		head->left = malloc(sizeof(struct node));
-		head->left->val = x;
-		head->left->left = NULL;
-		head->left->right = NULL;
-		
-	}else if(x>head->val){
-		if(head->right!=NULL){
-			insertbst(head->right,x);
-		}
-		printf("larger than curr node\n");
-		head->right = malloc(sizeof(struct node));
-		head->right->val = x;
-		head->right->left = NULL;
-		head->right->right = NULL;
-		
+struct node* insertbst(struct node* head,int x){
+	if(head==NULL){
+		head = malloc(sizeof(struct node));
+		head->val = x;
+		head->left = head->right = NULL;
 	}
-	
-	
+	else if(x<head->val){
+
+		printf("smaller than curr node\n");
+		head->left = insertbst(head->left,x);
+
+	}else if(x>head->val){
+
+		printf("larger than curr node\n");
+		head->right = insertbst(head->right,x);
+
+	}
+}
+
+void bstiter(struct node* node){
+
+
+}
+
+
+void padding ( char ch, int n ){
+  int i;
+
+  for ( i = 0; i < n; i++ )
+    putchar ( ch );
+}
+
+void structure ( struct node *root, int level ){
+  int i;
+
+  if ( root == NULL ) {
+    padding ( '\t', level );
+    puts ( "~" );
+  } else {
+    structure ( root->right, level + 1 );
+    padding ( '\t', level );
+    printf( "%d\n", root->val );
+    structure ( root->left, level + 1 );
+  }
+}
+
+void iterativemin(struct node* root){
+	struct node* curr = root;
+	while(curr->left!=NULL || curr->right!=NULL){
+		if(curr->left!=NULL){
+
+			curr = curr->left;
+		}else if(curr->right!=NULL){
+
+			curr = curr->right;
+		}else{
+
+		}
+	}
+	printf("min is: %d",curr->val);
+
+}
+
+void iterativemax(struct node* root){
+	struct node* curr = root;
+	while(curr->left!=NULL || curr->right!=NULL){
+		if(curr->right!=NULL){
+			curr = curr->right;
+		}else if(curr->left!=NULL){
+
+			curr = curr->left;
+		}else{
+
+		}
+	}
+	printf("max is: %d",curr->val);
+}
+
+int recursivemin(struct node* node){
+	if(node->left!=NULL){
+		return recursivemin(node->left);
+	}
+	return node->val;
+}
+
+int recursivemax(struct node* node){
+	if(node->right!=NULL){
+		return recursivemin(node->right);
+	}
+	return node->val;
+}
+
+int findelement(struct node* node,int val){
+    if(val<node->val){
+        findelement(node->left,val);
+    }else if(val>node->val){
+        findelement(node->right,val);
+    }else{
+        printf("found %d, memory pointer is %x\n",node->val,node);
+        return node;
+    }
 }
 
 
 void main(){
-    struct node* head = malloc(sizeof(struct node));
-    head->left = NULL;
-    head->right = NULL;
-    head->val = 8;
-    
+    struct node* root = malloc(sizeof(struct node));
+    root->left = NULL;
+    root->right = NULL;
+    root->val = 5;
+
     int x;
-    printf("Enter value: ");
-    scanf("%d",&x);
-    insertbst(head,x);
-    traverse(head);
-    
-    printf("Enter value: ");
-    scanf("%d",&x);
-    insertbst(head,x);
-    traverse(head);
-    
-    printf("Enter value: ");
-    scanf("%d",&x);
-    insertbst(head,x);
-    traverse(head);
-    
-    printf("Enter value: ");
-    scanf("%d",&x);
-    insertbst(head,x);
-    traverse(head);
-    
-    printf("Enter value: ");
-    scanf("%d",&x);
-    insertbst(head,x);
-    traverse(head);
-	
+	scanf("%d",&x);
+    insertbst(root,x);
+	scanf("%d",&x);
+    insertbst(root,x);
+	scanf("%d",&x);
+    insertbst(root,x);
+
+	structure(root,0);
+
+    //int* add = findelement(root,3);
+    //printf("\n\nelem at %x : %d",add,*add);
+
+    recursivemin(root);
+    recursivemax(root);
+    iterativemax(root);
+    iterativemin(root);
+
+
 }
